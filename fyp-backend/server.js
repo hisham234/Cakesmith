b3b1224b5dfd5522026e6f9b55136fe5b1d09452
Hisham Naeem <Hixham234@gmail.com>
2021-12-06 14:28:28 +0500

    publish to heruko

--- fyp-backend/server.js
+++ fyp-backend/server.js
@@ -35,9 +35,13 @@ app.get('/api/config/paypal', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+app.use(express.static(path.join(__dirname, 'fyp-frontend/build')));
+app.get('*', (req, res) =>
+   res.sendFile(path.join(__dirname, 'fyp-frontend/build/index.html'))
+);
+//app.get('/', (req, res) => {
+ // res.send('Server is ready');
+//});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
