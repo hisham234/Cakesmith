be826cbecfcd5892a788c448df6caaadf8c5e013
Hisham Naeem <Hixham234@gmail.com>
2021-12-06 15:49:42 +0500

    from Udemy

--- fyp-backend/server.js
+++ fyp-backend/server.js
@@ -2,30 +2,22 @@ import http from 'http';
 import { Server } from 'socket.io';
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
-import dotenv from 'dotenv';
 import orderRouter from './routers/orderRouter.js';
-import path from 'path';
 import uploadRouter from './routers/uploadRouter.js';
 
 
 
 dotenv.config();
 
-
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/cakesmith', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona');
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
@@ -33,20 +25,24 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+app.get('/api/config/google', (req, res) => {
+  res.send(process.env.GOOGLE_API_KEY || '');
+});
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, 'fyp-frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
-   res.sendFile(path.join(__dirname, 'fyp-frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
-//app.get('/', (req, res) => {
- // res.send('Server is ready');
-//});
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-const port =  process.env.PORT || '5000' 
+
+const port = process.env.PORT || 5000;
 
 const httpServer = http.Server(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
@@ -113,7 +109,7 @@ io.on('connection', (socket) => {
       } else {
         io.to(socket.id).emit('message', {
           name: 'Admin',
-          body: 'Sorry. No one is available right now',
+          body: 'Sorry. I am not online right now',
         });
       }
     }
@@ -121,6 +117,5 @@ io.on('connection', (socket) => {
 });
 
 httpServer.listen(port, () => {
-
   console.log(`Serve at http://localhost:${port}`);
 });
